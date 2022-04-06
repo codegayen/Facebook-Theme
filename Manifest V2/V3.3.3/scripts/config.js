b03ddb831b7cfac6d02e5eccfee4ff90a350beb8
Hemanta Gayen <69314427+codegayen@users.noreply.github.com>
2022-04-06 14:02:29 +0530

    update

---
+++ Manifest V2/V3.3.3/scripts/config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.log = false;
+
+config.welcome = {
+  set lastupdate (val) {app.storage.write("lastupdate", val)},
+  get lastupdate () {return app.storage.read("lastupdate") !== undefined ? app.storage.read("lastupdate") : 0}
+};
